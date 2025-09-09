511cc731101a36e6ea62cf7ca7a5a13b6a5373a2
mahedihasan20 <talhatahmid009@gmail.com>
2025-09-09 16:18:11 +0600

    2nd commit

---
+++ script.js
@@ -0,0 +1,11 @@
+var API_BASE = "https://openapi.programming-hero.com/api";
+
+// DOM Elements
+var categoryList = document.getElementById("category-list");
+var productList = document.getElementById("product-list");
+var cartItemsContainer = document.getElementById("cart-items");
+var cartTotal = document.getElementById("cart-total");
+var modal = document.getElementById("tree-modal");
+var closeModalBtn = document.getElementById("close-modal");
+var bottomCloseBtn = document.getElementById("bottom-close");
+
